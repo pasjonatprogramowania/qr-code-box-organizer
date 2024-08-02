5f8b9ac10070ef8d23f4a90b4d252306ba70c384
pasjonatprogramowania <44599809+pasjonatprogramowania@users.noreply.github.com>
2024-08-02 19:49:28 +0200

    add

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import QrcodeVue from 'qrcode-vue'
 import 'bulma/css/bulma.min.css'
+import './registerServiceWorker.js'
 
 const pinia = createPinia();
 const app = createApp(App);
